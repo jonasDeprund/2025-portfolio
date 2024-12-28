5ccb3af47667b5ebb22eea85e8a6bb14a1e282fc
Jonas Deprund <jonasdeprund@gmail.com>
2024-12-28 19:09:06 +0100

    header wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,15 @@ import React from 'react';
 function Header() {
   return (
     <div className="header">
-      {/* text */}
       <div className="row">
-        <div className="header__title text-selectable cell-start-0 cell-end-12 cell-start-0-m cell-end-12-m">
-          <h1 className="text-h1 text-h1-m ">
-            Hello,
-            <br></br>I am an interactive designer based in Paris.
-            <br></br>I craft identities and websites.
+        <div className="header__title text-selectable cell-start-0 cell-end-8 cell-start-0-m cell-end-12-m">
+          <h1 className="text-h1 text-h1-m">
+            Hello, I’m a designer based in Paris.I create user-focused
+            interfaces and digital experiences.
           </h1>
+        </div>
+        <div className="header__scroll cell-start-0 cell-end-12">
+          <span className="text-caption">↓ Scroll down</span>
         </div>
       </div>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,22 @@
 import React from 'react';
-
-import iconArrowRight from '../svg/arrow-right.svg';
+import logo from '../svg/logo.svg';
 
 function Nav() {
   return (
     <nav className="navigation">
       <ul className="row">
         <div className="navigation__item cell-start-0 cell-end-12">
-          <li className="">
-            <a className="text-h2 text-h2-m" href="index.html">
-              Jonas Deprund
+          <li>
+            <a className="logo" href="/">
+              <img src={logo} alt="logo" />
             </a>
           </li>
-          <li className="">
+          <li>
             <a
               className="contact button text-button"
               href="mailto:jonasdeprund@gmail.com"
             >
-              <span>Contact</span>
-              <img className="button__icon" src={iconArrowRight} alt="Arrow" />
+              Contact
             </a>
           </li>
         </div>
